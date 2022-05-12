c45ca38d38b89c862e0bb4f5930858d7ca5fa005
XINNNNN <105089262+xInDenGXD@users.noreply.github.com>
2022-05-11 21:50:50 -0400

    Update a-common-sense-guide-code-exercise.py

--- a-common-sense-guide-code-exercise.py
+++ a-common-sense-guide-code-exercise.py
@@ -334,3 +334,141 @@ l.print_all_node()
 l.reverse_the_list()
 l.print_all_node()
 
+#
+# 14 -3 ##################
+#
+
+## Node ##
+class Node:
+    def __init__(self, data, next_node):
+        self.data = data
+        self.next_node = next_node
+    def __str__(self):
+        if self.next_node:
+            return f"data is {self.data} and next_node is {self.next_node}"
+        else:
+            return f"data is {self.data} and next_node is None"
+            
+## linked list ##
+## read a 
+class LinkList:
+    def __init__(self, node):
+        self.first_node = node
+    
+    def read(self, index):
+        current_node = self.first_node
+        current_index = 0
+        while current_index < index:
+            if current_node.next_node:
+                current_node = current_node.next_node
+            else:
+                return None # cannot find the index
+        return current_node.data # return node data
+        
+    # search
+    def index_of(self, value):
+        current_node = self.first_node
+        current_index = 0
+        while current_node.data != value:
+            if current_node.next_node:
+                current_node = current_node.next_node
+            else:
+                return None # cannot find node contains value 
+        
+        return current_node.index # return index of the node
+    
+    # insert
+    def insert_at_index(self, index, value):
+        
+        if index == 0:
+            new_node = Node(value,self.first_node)
+            self.first_node = new_node
+            return
+        
+        current_node = self.first_node
+        current_index = 0
+        
+        while current_index < index-1:
+            if current_node.next_node:
+                current_node = current_node.next_node
+            else:
+                return None
+         
+        # current index-1
+        if current_node.next_node:
+            new_node = Node(value, current_node.next_node)
+            current_node.next_node = new_node  
+        else:
+            return None
+            
+        return "insertion success"
+    # deletion
+    def delete_at_index(self,index):
+        
+        if index == 0:
+            self.first_node = self.first_node.next_node
+            return
+        
+        current_node = self.first_node
+        current_index =0
+        
+        while current_index < index-1:
+            if current_node.next_node:
+                current_node = current_node.next_node
+            else:
+                return None
+        # at index-1
+        current_node.next_node = current_node.next_node.next_node
+        return "deletion success"
+       
+    def print_all_node(self):
+        if not self.first_node: # empty node case
+            return -1
+          
+        current_node = self.first_node
+        while current_node:
+            print(str(current_node.data))
+            current_node = current_node.next_node
+            
+    def return_the_last_node(self):
+        if not self.first_node: # empty node case
+            return -1
+        
+        current_node = self.first_node
+        while current_node.next_node:
+            current_node = current_node.next_node
+        
+        return current_node
+    
+    def reverse_the_list(self):
+        if not self.first_node:
+            return -1
+        
+        if not self.first_node.next_node: # only one node in the list / no need to change
+            return
+        
+        current_node = self.first_node.next_node
+        buff_node    = self.first_node.next_node
+        previous_node = self.first_node
+        
+        while current_node:
+            buff_node = current_node
+            current_node = current_node.next_node
+            buff_node.next_node = previous_node
+            previous_node = buff_node
+            
+            
+        self.first_node.next_node = None
+        self.first_node = previous_node
+##############
+# MAIN #######
+##############
+node_d = Node(3,None)
+node_c = Node(6,node_d)
+node_b = Node(2,node_c)
+node_a = Node(8,node_b)
+
+l = LinkList(node_a)
+l.print_all_node()
+l.reverse_the_list()
+l.print_all_node()
